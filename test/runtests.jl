bb6c1dd591cef6fb4e940deac99ce5a03fe616c4
Tom Lee <contact@tomlee.com.au>
2018-09-25 10:27:10 +1000

    Fix bug in tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -34,7 +34,7 @@ using Cookbook
 end
 
 for f in ["a", "b1", "b2", "c", "d1", "d2"]
-    rm("test/$f.txt", force=true)
+    rm("$f.txt", force=true)
 end
 
 header="this is the first line"
@@ -42,31 +42,31 @@ recipes = Recipe[]
 prepare!(
     recipes,
     MyRecipe,
-    a="test/a.txt",
-    b=["test/b1.txt", "test/b2.txt"],
-    c="test/c.txt",
-    d=["test/d1.txt", "test/d2.txt"],
+    a="a.txt",
+    b=["b1.txt", "b2.txt"],
+    c="c.txt",
+    d=["d1.txt", "d2.txt"],
     header=header,
 )
 
 c_content = "this is input \ntext file c"
-open("test/c.txt", "w") do f
+open("c.txt", "w") do f
     println(f, c_content)
 end
 d_content =  [ "this is input \ntext file d$i" for i in 1:2 ]
 for i in 1:2
-    open("test/d$i.txt", "w") do f
+    open("d$i.txt", "w") do f
         println(f, d_content[i])
     end
 end
 
 make(recipes)
 
-@test "$header\n$(reverse(c_content))\n" == open(x->read(x, String), "test/a.txt")
+@test "$header\n$(reverse(c_content))\n" == open(x->read(x, String), "a.txt")
 
 for i in 1:2
     expect = "$header\n$(reverse(d_content[i]))\n"
-    @test  expect == open(x->read(x, String), "test/b$i.txt")
+    @test  expect == open(x->read(x, String), "b$i.txt")
 end
 
 make(recipes)
